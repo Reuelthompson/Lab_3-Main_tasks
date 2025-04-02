c0fedc754dbffb354d2274b52a012ca3d3d4cc03
Reuelthompson <Reuel2.thompson@live.uwe.ac.uk>
2025-04-02 12:03:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,94 +1,136 @@
+//=====[Libraries]=============================================================
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalIn enterButton(BUTTON1);
+DigitalIn gasDetector(D2);
+DigitalIn overTempDetector(D3);
+DigitalIn aButton(D4);
+DigitalIn bButton(D5);
+DigitalIn cButton(D6);
+DigitalIn dButton(D7);
+
+DigitalOut alarmLed(LED1);
+DigitalOut incorrectCodeLed(LED3);
+DigitalOut systemBlockedLed(LED2);
+
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+//=====[Declaration and initialization of public global variables]=============
+
+bool alarmState = OFF;
+int numberOfIncorrectCodes = 0;
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void inputsInit();
+void outputsInit();
+
+void alarmActivationUpdate();
+void alarmDeactivationUpdate();
+
+void uartTask();
+void availableCommands();
+
+//=====[Main function, the program entry point after power on or reset]========
+
 int main()
 {
-    DigitalIn enterButton(BUTTON1);     //Initialise Buttons
-    DigitalIn gasDetector(D2);
-    DigitalIn overTempDetector(D3);
-    DigitalIn aButton(D4);
-    DigitalIn bButton(D5);
-    DigitalIn cButton(D6);
-    DigitalIn dButton(D7);
+    inputsInit();
+    outputsInit();
+    while (true) {
+        alarmActivationUpdate();
+        alarmDeactivationUpdate();
+        uartTask();
+    }
+}
 
-    DigitalOut alarmLed(LED1);          //Initialise LEDs
-    DigitalOut incorrectCodeLed(LED3);
-    DigitalOut systemBlockedLed(LED2);
+//=====[Implementations of public functions]===================================
 
-    gasDetector.mode(PullDown);         // dictate pull down or pull up connecction
+void inputsInit()
+{
+    gasDetector.mode(PullDown);
     overTempDetector.mode(PullDown);
     aButton.mode(PullDown);
     bButton.mode(PullDown);
     cButton.mode(PullDown);
     dButton.mode(PullDown);
+}
 
-    alarmLed = OFF;                     //Set initial state of LEDs
+void outputsInit()
+{
+    alarmLed = OFF;
     incorrectCodeLed = OFF;
     systemBlockedLed = OFF;
+}
 
-    int alarmState = OFF;
-    int numberOfIncorrectCodes = 0;
+void alarmActivationUpdate()
+{
+    if ( gasDetector || overTempDetector ) {
+        alarmState = ON;
+    }
+    alarmLed = alarmState;
+}
 
-    while (true) {                              //main loop to keep checking for an input
-        if (gasDetector && overTempDetector){   //If both are detected run the next bit of code
-            alarmState = ON;                    
-
-            while(alarmState == ON){            //Loop until the alarm state changes to false
-                alarmLed = !alarmLed;
-                rtos::ThisThread::sleep_for(100ms); //Command to set the LED to blink
-
-                if ( numberOfIncorrectCodes < 5 ) { //Check to see if the number of incorrect codes is less than 5
-            
-                    if ( aButton && bButton && cButton && dButton && !enterButton ) { // incorrect code reset
-                    incorrectCodeLed = OFF;
-                }
-                    if ( enterButton && !incorrectCodeLed && alarmState) { 
-                        if ( aButton && bButton && !cButton && !dButton ) { // passcode combination
-                            alarmState = OFF;
-                            numberOfIncorrectCodes = 0;
-                        } else {                                            //If incorrect password is input then turn LED on and add 1 to incorrect codes
-                            incorrectCodeLed = ON;
-                            numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
-                        }
-                    }
-                } else {
-                    for(int I = 0; I<120; I++){                 //for loop that goes for 120 500ms intervals turning the LED on and off
-                        systemBlockedLed = !systemBlockedLed;
-                        rtos::ThisThread::sleep_for(500ms);
-                    }
-                systemBlockedLed = OFF;                         //After the 60sec wait the user can go again because the codes is reset
+void alarmDeactivationUpdate()
+{
+    if ( numberOfIncorrectCodes < 5 ) {
+        if ( aButton && bButton && cButton && dButton && !enterButton ) {
+            incorrectCodeLed = OFF;
+        }
+        if ( enterButton && !incorrectCodeLed && alarmState ) {
+            if ( aButton && bButton && !cButton && !dButton ) {
+                alarmState = OFF;
                 numberOfIncorrectCodes = 0;
-                }
+            } else {
+                incorrectCodeLed = ON;
+                numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
             }
         }
-        if ( gasDetector || overTempDetector ) {                // if gas or overtemp are on
-            alarmState = ON;
+    } else {
+        systemBlockedLed = ON;
+    }
+}
+
+void uartTask()
+{
+    char receivedChar = '\0';
+    if( uartUsb.readable() ) {
+        uartUsb.read( &receivedChar, 1);
+        if ( receivedChar == '1') {
+            if ( alarmState == true ) {
+                uartUsb.write( "The alarm is activated\r\n", 24);
+            } else {
+                uartUsb.write( "The alarm is not activated\r\n", 28);
+            }
         }
-
-        alarmLed = alarmState;
-
-        if ( numberOfIncorrectCodes < 5 ) {
+        else if ( receivedChar == '2') {
+            if ( gasDetector == true ) {
+                uartUsb.write( "The Gas Detector is activated\r\n", 32);
+            } else {
+                uartUsb.write( "The Gas Detector is not activated\r\n", 36);
+            }
+        }
+        else if ( receivedChar == '3') {
+            if ( overTempDetector == true ) {
+                uartUsb.write( "The Temperature Sensor is activated\r\n", 38);
+            } else {
+                uartUsb.write( "The Temperature Sensor is not activated\r\n", 42);
+            } 
             
-            if ( aButton && bButton && cButton && dButton && !enterButton ) {
-                incorrectCodeLed = OFF;
-            }
-
-            if ( enterButton && !incorrectCodeLed && alarmState) {
-                if ( aButton && bButton && !cButton && !dButton ) {
-                    alarmState = OFF;
-                    numberOfIncorrectCodes = 0;
-                } else {
-                    incorrectCodeLed = ON;
-                    numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
-                }
-            }
         } else {
-            for(int I = 0; I<120; I++){
-                    systemBlockedLed = !systemBlockedLed;
-                    rtos::ThisThread::sleep_for(500ms);
-                }
-        systemBlockedLed = OFF;
-        numberOfIncorrectCodes = 0;
-            }
+            availableCommands();
         }
     }
+}
+
+void availableCommands()
+{
+    uartUsb.write( "Available commands:\r\n", 21 );
+    uartUsb.write( "Press '1' to get the alarm state\r\n\r\n", 36 );
+    uartUsb.write( "Press '2' to get the Gas Sensor state\r\n\r\n", 42 );
+    uartUsb.write( "Press '3' to get the Temperature Sensor state\r\n\r\n", 48 );
+}
